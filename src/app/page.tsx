77ff66b4528657b3129cfb6ad927c54e0d0dd5cb
Oscar jeremias jc <oscarjeremias3@gmail.com>
2023-01-23 05:39:15 +0000

    prisma add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-export default function Home() {
+
+export default async function Home() {
     return (
         <h1 className="text-center">Ola mundo</h1>
+
     )
 }
--- src/repositories/user/create_user.ts
+++ src/repositories/user/create_user.ts
@@ -1,5 +1,12 @@
 import { IUser } from "../../entities/user"
+import { PrismaClient } from "@prisma/client"
 
 export async function create_user(user: IUser) {
-    
+    const prisma = new PrismaClient()
+
+   const res = await prisma.user.create({
+    data: user
+   })
+
+   return res
 }
